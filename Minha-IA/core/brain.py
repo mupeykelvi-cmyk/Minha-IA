8f9a5a6d1dee80c61c6ded43b7853136e581c076
mupeykelvi <kelvimupey@gmail.com>
2026-07-18 16:15:44 +0000

    Aurora AI com sistema de aprendizado e memoria

--- Minha-IA/core/brain.py
+++ Minha-IA/core/brain.py
@@ -1,18 +1,28 @@
+from core.memory import aprender, lembrar
+
+
 def responder(mensagem, memoria):
 
     texto = mensagem.lower()
 
-    if "nome" in texto:
-        return "Meu nome é Aurora AI."
+    if "eu gosto de" in texto:
+        assunto = texto.replace("eu gosto de", "").strip()
+        aprender("gosto", assunto, memoria)
+        return f"Vou lembrar que você gosta de {assunto}."
+
+    elif "do que eu gosto" in texto:
+        gosto = lembrar("gosto", memoria)
+
+        if gosto:
+            return f"Você gosta de {gosto}."
+
+        return "Ainda não sei do que você gosta."
 
     elif "olá" in texto or "ola" in texto:
         return f"Olá, {memoria.get('usuario')}!"
 
     elif "quem sou eu" in texto:
         return f"Você é {memoria.get('usuario')}."
-
-    elif texto == memoria.get("usuario", "").lower():
-        return f"Sim, eu lembro de você, {memoria.get('usuario')}."
 
     else:
         return "Ainda estou aprendendo essa informação."
--- Minha-IA/core/memory.py
+++ Minha-IA/core/memory.py
@@ -3,12 +3,31 @@ import os
 
 ARQUIVO = "data/memory.json"
 
+
 def carregar_memoria():
     if os.path.exists(ARQUIVO):
         with open(ARQUIVO, "r") as arquivo:
-            return json.load(arquivo)
-    return {}
+            memoria = json.load(arquivo)
+
+            if "conhecimento" not in memoria:
+                memoria["conhecimento"] = {}
+
+            return memoria
+
+    return {
+        "conhecimento": {}
+    }
+
 
 def salvar_memoria(memoria):
     with open(ARQUIVO, "w") as arquivo:
         json.dump(memoria, arquivo, indent=4)
+
+
+def aprender(chave, valor, memoria):
+    memoria["conhecimento"][chave] = valor
+    salvar_memoria(memoria)
+
+
+def lembrar(chave, memoria):
+    return memoria["conhecimento"].get(chave)
