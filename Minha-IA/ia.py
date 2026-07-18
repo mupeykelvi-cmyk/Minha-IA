b019fceccacd6fbb1b5439b9fa91bbea8f29d091
mupeykelvi <kelvimupey@gmail.com>
2026-07-18 15:49:02 +0000

    Estruturei Aurora AI como projeto modular

--- Minha-IA/ia.py
+++ Minha-IA/ia.py
@@ -2,15 +2,26 @@ nome_ia = "Aurora"
 
 print(f"Olá! Eu sou {nome_ia}, uma IA simples.")
 
-nome_usuario = input("Qual é o seu nome? ")
+try:
+    with open("memoria.txt", "r") as arquivo:
+        nome_usuario = arquivo.read().strip()
 
-print(f"Prazer em conhecer você, {nome_usuario}!")
+except:
+    nome_usuario = ""
+
+if nome_usuario == "":
+    nome_usuario = input("Qual é o seu nome? ")
+
+    with open("memoria.txt", "w") as arquivo:
+        arquivo.write(nome_usuario)
+
+print(f"Prazer em falar com você, {nome_usuario}!")
 
 while True:
     pergunta = input(f"{nome_usuario}: ")
 
     if pergunta.lower() == "olá":
-        print(f"{nome_ia}: Olá, {nome_usuario}! Como posso ajudar?")
+        print(f"{nome_ia}: Olá, {nome_usuario}!")
 
     elif pergunta.lower() == "sair":
         print(f"{nome_ia}: Até logo, {nome_usuario}!")
@@ -18,4 +29,3 @@ while True:
 
     else:
         print(f"{nome_ia}: Ainda estou aprendendo.")
-
