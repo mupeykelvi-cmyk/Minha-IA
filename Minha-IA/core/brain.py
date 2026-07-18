451d2c124e2202f86994ec248ecff11711a81248
mupeykelvi <kelvimupey@gmail.com>
2026-07-18 16:25:07 +0000

    Melhorei memoria e entendimento da Aurora AI

--- Minha-IA/core/brain.py
+++ Minha-IA/core/brain.py
@@ -5,24 +5,47 @@ def responder(mensagem, memoria):
 
     texto = mensagem.lower()
 
-    if "eu gosto de" in texto:
-        assunto = texto.replace("eu gosto de", "").strip()
-        aprender("gosto", assunto, memoria)
-        return f"Vou lembrar que você gosta de {assunto}."
+    if texto.startswith("meu ") and " é " in texto:
 
-    elif "do que eu gosto" in texto:
-        gosto = lembrar("gosto", memoria)
+        partes = texto.replace("meu ", "").split(" é ")
 
-        if gosto:
-            return f"Você gosta de {gosto}."
+        chave = partes[0].strip()
+        valor = partes[1].strip()
 
-        return "Ainda não sei do que você gosta."
+        aprender(chave, valor, memoria)
 
-    elif "olá" in texto or "ola" in texto:
-        return f"Olá, {memoria.get('usuario')}!"
+        return f"Vou lembrar que seu {chave} é {valor}."
 
-    elif "quem sou eu" in texto:
+
+    if "qual" in texto and "meu" in texto:
+
+        chave = texto
+
+        chave = chave.replace("qual é o meu ", "")
+        chave = chave.replace("qual é meu ", "")
+        chave = chave.replace("qual meu ", "")
+        chave = chave.replace("qual éo meu ", "")
+        chave = chave.replace("?", "")
+        chave = chave.strip()
+
+        valor = lembrar(chave, memoria)
+
+        if valor:
+            return f"Seu {chave} é {valor}."
+
+        return f"Ainda não sei qual é seu {chave}."
+
+
+    if "nome" in texto:
+        return "Meu nome é Aurora AI."
+
+
+    if "quem sou eu" in texto:
         return f"Você é {memoria.get('usuario')}."
 
-    else:
-        return "Ainda estou aprendendo essa informação."
+
+    if "olá" in texto or "ola" in texto:
+        return f"Olá, {memoria.get('usuario')}!"
+
+
+    return "Ainda estou aprendendo essa informação."
